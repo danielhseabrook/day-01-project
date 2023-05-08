a38c2e5e85f08aff81b9f63efdeefa120694d19a
danielhseabrook <daniel.seabrook93@gmail.com>
2023-05-08 16:12:36 +1000

    Initial commit

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city_born=input("What city you born in?\n")
+first_pet=input("What was the name of your first pet?\n")
+print("Your band name is The" " " + city_born + " " + first_pet + "s")
